7c19b7cc583b8e9cb62b0dc43e68137c34e8c9be
Bruno <brunobrito728@gmail.com>
2022-10-31 16:30:22 -0300

    finalizado o requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import submitEmail from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    this.validationEmail();
+  };
+
+  validationEmail = () => {
+    const six = 6;
+    const regex = /\S+@\S+\.\S+/;
+    const { email, password } = this.state;
+    const emailRegex = regex.test(email);
+    return !(emailRegex && password.length >= six);
+  };
+
+  clickButton = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(submitEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <input
+          type="tex"
+          data-testid="email-input"
+          placeholder="email"
+          name="email"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          placeholder="senha"
+          name="password"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ this.validationEmail() }
+          onClick={ this.clickButton }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+const submitEmail = (email) => ({
+  type: 'USE_EMAIL',
+  payload: email,
+});
+
+export default submitEmail;
