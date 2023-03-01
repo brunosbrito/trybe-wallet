b499dccd614494b0ee2fe1eb1c390efec1aaa139
brunosbrito <brunobrito728@gmail.com>
2023-03-01 16:24:01 -0300

    [feat] realizado deploy da aplicação

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -12,10 +12,10 @@ ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <Provider store={ store }>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <GlobalStyle />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>,
   );
 
